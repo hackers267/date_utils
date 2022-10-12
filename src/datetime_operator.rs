b359d10ea5b776949e179605ddcae23ac56fe73e
silence_zhpf <silence_zhpf@aliyun.com>
2022-10-12 17:16:47 +0800

    feat: ✨ 添加end系列方法
    ✨ 添加end_of_day方法
    ✨ 添加end_of_hour方法
    ✨ 添加end_of_minute方法

--- src/datetime_operator.rs
+++ src/datetime_operator.rs
@@ -4,6 +4,9 @@ pub trait DateTimeOperator {
     fn begin_of_day(&self) -> Self;
     fn begin_of_hour(&self) -> Self;
     fn begin_of_minute(&self) -> Self;
+    fn end_of_day(&self) -> Self;
+    fn end_of_hour(&self) -> Self;
+    fn end_of_minute(&self) -> Self;
 }
 
 impl<Tz: TimeZone> DateTimeOperator for DateTime<Tz> {
@@ -18,5 +21,17 @@ impl<Tz: TimeZone> DateTimeOperator for DateTime<Tz> {
 
     fn begin_of_minute(&self) -> Self {
         self.with_second(0).unwrap()
+    }
+
+    fn end_of_day(&self) -> Self {
+        self.date().and_hms(23, 59, 59)
+    }
+
+    fn end_of_hour(&self) -> Self {
+        self.with_minute(59).unwrap().with_second(59).unwrap()
+    }
+
+    fn end_of_minute(&self) -> Self {
+        self.with_second(59).unwrap()
     }
 }
--- tests/datetime_operator.rs
+++ tests/datetime_operator.rs
@@ -15,3 +15,34 @@ fn test_begin_of_hour() {
     let begin = Utc.ymd(2008, 8, 8).and_hms(8, 0, 0);
     assert_eq!(begin, result);
 }
+#[test]
+fn test_begin_of_minute() {
+    let datetime = Utc.ymd(2008, 8, 8).and_hms(8, 8, 8);
+    let result = datetime.begin_of_minute();
+    let begin = Utc.ymd(2008, 8, 8).and_hms(8, 8, 0);
+    assert_eq!(begin, result);
+}
+
+#[test]
+fn test_end_of_day() {
+    let datetime = Utc.ymd(2008, 8, 8).and_hms(8, 8, 8);
+    let result = datetime.end_of_day();
+    let end = Utc.ymd(2008, 8, 8).and_hms(23, 59, 59);
+    assert_eq!(result, end)
+}
+
+#[test]
+fn test_end_of_hour() {
+    let datetime = Utc.ymd(2008, 8, 8).and_hms(8, 8, 8);
+    let result = datetime.end_of_hour();
+    let end = Utc.ymd(2008, 8, 8).and_hms(8, 59, 59);
+    assert_eq!(result, end);
+}
+
+#[test]
+fn test_end_of_minute() {
+    let datetime = Utc.ymd(2008, 8, 8).and_hms(8, 8, 8);
+    let result = datetime.end_of_minute();
+    let end = Utc.ymd(2008, 8, 8).and_hms(8, 8, 59);
+    assert_eq!(result, end);
+}
