ac91ff6c6554288cdcf84572e38fa1822c77fe73
silence_zhpf <silence_zhpf@aliyun.com>
2022-10-12 14:59:50 +0800

    feat: ✨ 添加begin_of_hour方法

--- src/datetime_operator.rs
+++ src/datetime_operator.rs
@@ -1,11 +1,17 @@
-use chrono::{DateTime, TimeZone};
+use chrono::{DateTime, TimeZone, Timelike};
 
 pub trait DateTimeOperator {
     fn begin_of_day(&self) -> Self;
+    fn begin_of_hour(&self) -> Self;
 }
 
 impl<Tz: TimeZone> DateTimeOperator for DateTime<Tz> {
     fn begin_of_day(&self) -> Self {
         self.date().and_hms(0, 0, 0)
+    }
+
+    fn begin_of_hour(&self) -> Self {
+        let hour = self.hour();
+        self.date().and_hms(hour, 0, 0)
     }
 }
--- tests/datetime_operator.rs
+++ tests/datetime_operator.rs
@@ -8,3 +8,10 @@ fn test_begin_of_day() {
     let result = Utc.ymd(2008, 8, 8).and_hms(0, 0, 0);
     assert_eq!(begin, result)
 }
+#[test]
+fn test_begin_of_hour() {
+    let datetime = Utc.ymd(2008, 8, 8).and_hms(8, 8, 8);
+    let result = datetime.begin_of_hour();
+    let begin = Utc.ymd(2008, 8, 8).and_hms(8, 0, 0);
+    assert_eq!(begin, result);
+}
