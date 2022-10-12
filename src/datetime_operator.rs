60bcebe341c05df8b1c5a7621c18010380a256c3
silence_zhpf <silence_zhpf@aliyun.com>
2022-10-12 17:09:31 +0800

    feat: ✨ 添加begin_of_minute方法

--- src/datetime_operator.rs
+++ src/datetime_operator.rs
@@ -3,6 +3,7 @@ use chrono::{DateTime, TimeZone, Timelike};
 pub trait DateTimeOperator {
     fn begin_of_day(&self) -> Self;
     fn begin_of_hour(&self) -> Self;
+    fn begin_of_minute(&self) -> Self;
 }
 
 impl<Tz: TimeZone> DateTimeOperator for DateTime<Tz> {
@@ -13,5 +14,9 @@ impl<Tz: TimeZone> DateTimeOperator for DateTime<Tz> {
     fn begin_of_hour(&self) -> Self {
         let hour = self.hour();
         self.date().and_hms(hour, 0, 0)
+    }
+
+    fn begin_of_minute(&self) -> Self {
+        self.with_second(0).unwrap()
     }
 }
