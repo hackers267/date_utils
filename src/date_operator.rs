c6aa0d040d6212002183cbe3e2a050d553f3d0cb
silence_zhpf <silence_zhpf@aliyun.com>
2023-08-08 14:07:22 +0800

    ✨ feat: 更新chrono到0.4.26版本

--- src/date_operator.rs
+++ src/date_operator.rs
@@ -1,5 +1,5 @@
-use chrono::Datelike;
 pub use chrono::{Date, DateTime, TimeZone};
+use chrono::{Datelike, NaiveDate};
 
 pub trait DateOperator {
     fn years_since(&self, base: Self) -> Option<u32>;
@@ -400,5 +400,190 @@ impl<Tz: TimeZone> DateOperator for DateTime<Tz> {
     /// ```
     fn is_leap_year(&self) -> bool {
         self.date().is_leap_year()
+    }
+}
+impl DateOperator for NaiveDate {
+    fn years_since(&self, base: Self) -> Option<u32> {
+        self.years_since(base)
+    }
+
+    /// To check the current date  is before the given date or not.
+    /// 判断当前日期是否在指定的日期前。
+    ///
+    /// # Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let other = Utc.ymd(2008,1,1);
+    /// assert!(!date.before(&other));
+    /// ```
+    fn before(&self, other: &Self) -> bool {
+        self < other
+    }
+
+    /// To check the current date is after the given date or datetime or not.
+    /// 判断当前刵是是否在指定的日期后。
+    ///
+    /// # Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let other = Utc.ymd(2006,6,6);
+    /// assert!(date.after(&other));
+    /// ```
+    fn after(&self, other: &Self) -> bool {
+        self > other
+    }
+
+    /// To check the current date is the same date with the given date or not.
+    /// 判断当前日期和指定的日期是否在同一天
+    ///
+    /// #Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let other = Utc.ymd(2006,6,6);
+    /// assert!(!date.is_same_day(&other))
+    /// ```
+    ///
+    fn is_same_day(&self, other: &Self) -> bool {
+        self == other
+    }
+
+    /// To check the current date is the same date with the given date or not.
+    /// 判断当前日期和指定的日期是否在同一个月
+    ///
+    /// # Example 示例
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let other = Utc.ymd(2008,8,31);
+    /// assert!(date.is_same_month(&other));
+    /// ```
+    fn is_same_month(&self, other: &Self) -> bool {
+        self.year() == other.year() && self.month() == other.month()
+    }
+
+    /// To check whether the other date is the same year with the date.
+    /// 判断另一个日期是否和当前日期在同一年
+    ///
+    /// # Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let other = Utc.ymd(2008,1,1);
+    /// assert!(date.is_same_year(&other))
+    /// ```
+    fn is_same_year(&self, other: &Self) -> bool {
+        self.year() == other.year()
+    }
+
+    /// Get the start date of a year.
+    /// 获取一年的开始日期
+    /// # Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let result = date.begin_of_year();
+    /// let begin = Utc.ymd(2008,1,1);
+    /// assert_eq!(begin,result);
+    /// ```
+    fn begin_of_year(&self) -> Self {
+        self.with_month(1).unwrap().with_day(1).unwrap()
+    }
+
+    /// Get the end of year.
+    /// 获取一年的结束日期。
+    /// # Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let result = date.end_of_year();
+    /// let end = Utc.ymd(2008,12,31);
+    /// assert_eq!(result, end);
+    /// ```
+    fn end_of_year(&self) -> Self {
+        self.with_month(12).unwrap().with_day(31).unwrap()
+    }
+
+    /// Get the begin date of a month.
+    /// 获取某个月的开始日
+    ///
+    /// # Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let result = date.begin_of_month();
+    /// let begin = Utc.ymd(2008,8,1);
+    /// assert_eq!(result,begin);
+    /// ```
+    fn begin_of_month(&self) -> Self {
+        self.with_day(1).unwrap()
+    }
+
+    /// Get the end of month.
+    /// 获取某个月的结束日期
+    ///
+    /// # Example 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date = Utc.ymd(2008,8,8);
+    /// let result = date.end_of_month();
+    /// let end = Utc.ymd(2008,8,31);
+    /// assert_eq!(result,end);
+    /// ```
+    fn end_of_month(&self) -> Self {
+        let day31 = [1, 3, 5, 7, 8, 10, 12];
+        let day30 = [4, 6, 9, 11];
+        let month = self.month();
+        if day31.binary_search(&month).is_ok() {
+            return self.with_day(31).unwrap();
+        }
+        if day30.binary_search(&month).is_ok() {
+            return self.with_day(30).unwrap();
+        }
+        if self.is_leap_year() {
+            return self.with_day(29).unwrap();
+        }
+        self.with_day(28).unwrap()
+    }
+
+    /// To check the date's year is leap year or not.
+    /// 判断日期所在的年份是可是闰年
+    ///
+    /// #  Examples 示例
+    ///
+    /// ```
+    /// use chrono::{TimeZone, Utc};
+    /// use date_utils::DateOperator;
+    /// let date =  Utc.ymd(2008,8,8);
+    /// assert!(date.is_leap_year());
+    /// ```
+    fn is_leap_year(&self) -> bool {
+        let year = self.year();
+        if year % 400 == 0 {
+            return true;
+        }
+        if year % 4 == 0 {
+            return true;
+        }
+        false
     }
 }
--- src/period.rs
+++ src/period.rs
@@ -1,5 +1,5 @@
 use crate::DateOperator;
-use chrono::{Date, Datelike, TimeZone};
+use chrono::{Datelike, NaiveDate};
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub struct Period {
@@ -14,12 +14,11 @@ pub struct Period {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use chrono::Local;
     #[test]
     fn period_between_simple_test() {
-        let start = Local.ymd(2022, 2, 1);
-        let end = Local.ymd(2023, 3, 2);
-        let period = Period::bewteen(start, end);
+        let start = calc_date(2022, 2, 1).unwrap();
+        let end = calc_date(2023, 3, 2).unwrap();
+        let period = Period::bewteen(&start, &end);
         let year = period.year;
         let month = period.month;
         let day = period.day;
@@ -28,11 +27,15 @@ mod tests {
         assert_eq!(day, 1);
     }
 
+    fn calc_date(year: i32, month: u32, day: u32) -> Option<NaiveDate> {
+        NaiveDate::from_ymd_opt(year, month, day)
+    }
+
     #[test]
     fn period_between_less_one_month_test() {
-        let start = Local.ymd(2022, 2, 10);
-        let end = Local.ymd(2022, 3, 9);
-        let period = Period::bewteen(start, end);
+        let start = calc_date(2022, 2, 10).unwrap();
+        let end = calc_date(2022, 3, 9).unwrap();
+        let period = Period::bewteen(&start, &end);
         let year = period.year;
         let month = period.month;
         let day = period.day;
@@ -42,9 +45,9 @@ mod tests {
     }
     #[test]
     fn period_between_test() {
-        let start = Local.ymd(2022, 8, 28);
-        let end = Local.ymd(2035, 3, 10);
-        let period = Period::bewteen(start, end);
+        let start = calc_date(2022, 8, 28).unwrap();
+        let end = calc_date(2035, 3, 10).unwrap();
+        let period = Period::bewteen(&start, &end);
         assert_eq!(period.year, 12);
         assert_eq!(period.month, 6);
         assert_eq!(period.day, 10);
@@ -53,13 +56,10 @@ mod tests {
 
 impl Period {
     /// 计算两个日期之间的间隔，使用x年x月x日的记录方式
-    pub fn bewteen<Tz>(one: Date<Tz>, other: Date<Tz>) -> Period
-    where
-        Tz: TimeZone,
-    {
-        let (another, year) = Self::calc_year(one, &other);
-        let (another, month) = Self::calc_month(&other, another);
-        let day = Self::calc_day(other, another);
+    pub fn bewteen(one: &NaiveDate, other: &NaiveDate) -> Period {
+        let (another, year) = Self::calc_year(one, other);
+        let (another, month) = Self::calc_month(other, another);
+        let day = Self::calc_day(other, &another);
 
         Period {
             year,
@@ -71,18 +71,12 @@ impl Period {
         }
     }
 
-    fn calc_day<Tz>(other: Date<Tz>, another: Date<Tz>) -> i8
-    where
-        Tz: TimeZone,
-    {
-        let duration = other - another;
+    fn calc_day(other: &NaiveDate, another: &NaiveDate) -> i8 {
+        let duration = *other - *another;
         duration.num_days() as i8
     }
 
-    fn calc_month<Tz>(other: &Date<Tz>, another: Date<Tz>) -> (Date<Tz>, i8)
-    where
-        Tz: TimeZone,
-    {
+    fn calc_month(other: &NaiveDate, another: NaiveDate) -> (NaiveDate, i8) {
         let month = (other.month() + 12 - another.month()) % 12;
         let total = (another.month() + month) % 12;
         let mut another = another.with_month(total).unwrap();
@@ -95,10 +89,7 @@ impl Period {
         (another, month)
     }
 
-    fn calc_year<Tz>(one: Date<Tz>, other: &Date<Tz>) -> (Date<Tz>, i16)
-    where
-        Tz: TimeZone,
-    {
+    fn calc_year(one: &NaiveDate, other: &NaiveDate) -> (NaiveDate, i16) {
         let year = other.year() - one.year();
         let another = one.with_year(one.year() + year).unwrap();
         let year = if another.after(other) { year - 1 } else { year } as i16;
