78973f5f61254c8c9632e87851f0764ed1f4e10e
silence_zhpf <silence_zhpf@aliyun.com>
2023-11-23 11:01:41 +0800

    ✨ 添加新特型 `is_future` 和 `is_past`

--- src/common.rs
+++ src/common.rs
@@ -1,4 +1,4 @@
-use chrono::{NaiveDate, NaiveDateTime};
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
 
 pub trait CommonHelper {
     /// English: To check the current date or datetime is before the given date or datetime or not.
@@ -15,6 +15,16 @@ pub trait CommonHelper {
     ///
     /// 中文: 判断当前日期或时间是否和给定的日期或时间相等
     fn is_same(&self, other: &Self) -> bool;
+
+    /// English: To check the date or datetime is future or not.
+    ///
+    /// 中文: 判断日期或时间是否是未来
+    fn is_future(&self) -> bool;
+
+    /// English: To check the date or datetime is past or not.
+    ///
+    /// 中文: 判断日期或时间是否在过去
+    fn is_past(&self) -> bool;
 }
 
 impl CommonHelper for NaiveDate {
@@ -29,6 +39,16 @@ impl CommonHelper for NaiveDate {
     fn is_same(&self, other: &Self) -> bool {
         self == other
     }
+
+    fn is_future(&self) -> bool {
+        let now = Utc::now().date_naive();
+        self > &now
+    }
+
+    fn is_past(&self) -> bool {
+        let now = Utc::now().date_naive();
+        self < &now
+    }
 }
 
 impl CommonHelper for NaiveDateTime {
@@ -42,5 +62,15 @@ impl CommonHelper for NaiveDateTime {
 
     fn is_same(&self, other: &Self) -> bool {
         self == other
+    }
+
+    fn is_future(&self) -> bool {
+        let now = Utc::now().naive_utc();
+        self > &now
+    }
+
+    fn is_past(&self) -> bool {
+        let now = Utc::now().naive_utc();
+        self < &now
     }
 }
