e8ab7e5bbd9d1d4b9f25593ba0aa6c430610969c
silence_zhpf <silence_zhpf@aliyun.com>
2023-12-16 11:21:50 +0800

    ✨ 添加秒助手

--- src/second.rs
+++ src/second.rs
@@ -1,5 +1,7 @@
 use chrono::Duration;
+use chrono::NaiveDate;
 use chrono::NaiveDateTime;
+use chrono::Timelike;
 use std::marker;
 
 pub trait SecondHelper {
@@ -8,6 +10,10 @@ pub trait SecondHelper {
         Self: marker::Sized;
 
     fn sub_second(&self, second: i64) -> Option<Self>
+    where
+        Self: marker::Sized;
+    fn difference_is_second(&self, other: &Self) -> i64;
+    fn begin_of_second(&self) -> Self
     where
         Self: marker::Sized;
 }
@@ -19,6 +25,22 @@ impl SecondHelper for NaiveDateTime {
 
     fn sub_second(&self, second: i64) -> Option<Self> {
         self.checked_sub_signed(Duration::seconds(second))
+    }
+
+    fn difference_is_second(&self, other: &Self) -> i64 {
+        self.timestamp() - other.timestamp()
+    }
+
+    fn begin_of_second(&self) -> Self
+    where
+        Self: marker::Sized,
+    {
+        let date = self.date();
+        let time = self.time();
+        let hour = time.hour();
+        let minute = time.minute();
+        let second = time.second();
+        date.and_hms_milli_opt(hour, minute, second, 0).unwrap()
     }
 }
 
@@ -46,5 +68,25 @@ mod tests {
         let result = date_time.and_then(|date_time| date_time.sub_second(20));
         let actual = get_time(1999, 12, 31, 23, 59, 40);
         assert_eq!(result, actual);
+    }
+
+    #[test]
+    fn test_difference_second() {
+        let one = get_time(2000, 1, 1, 0, 0, 0);
+        let other = get_time(1999, 12, 31, 23, 59, 31);
+        let result = one.and_then(|time| other.map(|time1| time.difference_is_second(&time1)));
+        let actual = Some(29);
+        assert_eq!(result, actual)
+    }
+
+    #[test]
+    fn test_begin_of_second() {
+        let time = NaiveTime::from_hms_milli_opt(0, 0, 0, 300);
+        let date = NaiveDate::from_ymd_opt(2000, 1, 1);
+        let date_time = date.and_then(|date| time.map(|time| date.and_time(time)));
+        let result = date_time.map(|date_time| date_time.begin_of_second());
+        let actual =
+            NaiveDate::from_ymd_opt(2000, 1, 1).and_then(|date| date.and_hms_milli_opt(0, 0, 0, 0));
+        assert_eq!(result, actual)
     }
 }
