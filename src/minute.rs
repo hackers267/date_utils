49d2924dfb9af4c7347e8f65ae316620fd0c5e44
silence_zhpf <silence_zhpf@aliyun.com>
2023-12-27 10:49:45 +0800

    ✨ minute添加diff_minutes方法

--- src/minute.rs
+++ src/minute.rs
@@ -6,10 +6,19 @@ use chrono::{Duration, NaiveDateTime, Timelike};
 ///
 /// 中文: 分钟助手
 pub trait MinuteHelper {
+    /// English: Add the specified number of minutes
+    ///
+    /// 中文：加上指定的分钟数
+    fn add_minutes(&self, minute: u32) -> Self;
     /// English: Get the begin of one minute
     ///
     /// 中文: 获取分钟的开始时间
     fn begin_of_minute(&self) -> Self;
+    /// English: Get the signed number of full minutes
+    /// between the given dates.
+    ///
+    /// 中文：获取给定时间之间的分钟差
+    fn diff_minutes(&self, other: &Self) -> i64;
     /// English: Get the end of one minute
     ///
     /// 中文: 获取分钟的结束时间
@@ -18,15 +27,19 @@ pub trait MinuteHelper {
     ///
     /// 中文: 判断两个时间是否在同一分钟
     fn is_same_minute(&self, other: &Self) -> bool;
-    /// English: Add the specified number of minutes
-    ///
-    /// 中文：加上指定的分钟数
-    fn add_minutes(&self, minute: u32) -> Self;
 }
 
 impl MinuteHelper for NaiveDateTime {
+    fn add_minutes(&self, minute: u32) -> Self {
+        self.add(Duration::minutes(minute as i64))
+    }
+
     fn begin_of_minute(&self) -> Self {
         self.with_second(0).unwrap()
+    }
+
+    fn diff_minutes(&self, other: &Self) -> i64 {
+        self.signed_duration_since(other.to_owned()).num_minutes()
     }
 
     fn end_of_minute(&self) -> Self {
@@ -35,10 +48,6 @@ impl MinuteHelper for NaiveDateTime {
 
     fn is_same_minute(&self, other: &Self) -> bool {
         (self.timestamp() / 60) == (other.timestamp() / 60)
-    }
-
-    fn add_minutes(&self, minute: u32) -> Self {
-        self.add(Duration::minutes(minute as i64))
     }
 }
 
@@ -50,23 +59,41 @@ mod tests {
 
     #[test]
     fn test_same_minute_true() {
-        let one = NaiveDate::from_ymd_opt(2000, 1, 1).and_then(|date| date.and_hms_opt(0, 0, 0));
-        let other = NaiveDate::from_ymd_opt(2000, 1, 1).and_then(|date| date.and_hms_opt(0, 0, 1));
+        let one = get_time(2000, 1, 1, 0, 0, 0);
+        let other = get_time(2000, 1, 1, 0, 0, 1);
         assert!(one.unwrap().is_same_minute(&other.unwrap()));
+    }
+
+    fn get_time(
+        year: i32,
+        month: u32,
+        day: u32,
+        hour: u32,
+        min: u32,
+        sec: u32,
+    ) -> Option<NaiveDateTime> {
+        NaiveDate::from_ymd_opt(year, month, day).and_then(|date| date.and_hms_opt(hour, min, sec))
     }
     #[test]
     fn test_same_minute_false() {
-        let one = NaiveDate::from_ymd_opt(2000, 1, 1).and_then(|date| date.and_hms_opt(0, 0, 0));
-        let other = NaiveDate::from_ymd_opt(2000, 1, 1).and_then(|date| date.and_hms_opt(0, 1, 0));
+        let one = get_time(2000, 1, 1, 0, 0, 0);
+        let other = get_time(2000, 1, 1, 0, 1, 0);
         assert!(!one.unwrap().is_same_minute(&other.unwrap()));
     }
 
     #[test]
     fn test_add_minutes() {
-        let one = NaiveDate::from_ymd_opt(2000, 1, 1).and_then(|date| date.and_hms_opt(0, 0, 0));
+        let one = get_time(2000, 1, 1, 0, 0, 0);
+        let actual = get_time(2000, 1, 1, 0, 30, 0);
         let result = one.map(|date| date.add_minutes(30));
-        let actual =
-            NaiveDate::from_ymd_opt(2000, 1, 1).and_then(|date| date.and_hms_opt(0, 30, 0));
         assert_eq!(result, actual)
+    }
+
+    #[test]
+    fn test_diff_minutes() {
+        let one = get_time(2000, 1, 1, 0, 0, 0);
+        let other = get_time(2000, 1, 1, 0, 30, 0);
+        let diff = other.and_then(|first| one.map(|second| first.diff_minutes(&second)));
+        assert_eq!(diff, Some(30));
     }
 }
