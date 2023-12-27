0bfe7582973c55083f25479188bd75cb3b546fcf
silence_zhpf <silence_zhpf@aliyun.com>
2023-12-28 06:50:54 +0800

    ✨ 添加add_hours方法

--- src/hour.rs
+++ src/hour.rs
@@ -1,9 +1,14 @@
-use chrono::{NaiveDateTime, Timelike};
+use chrono::{Duration, NaiveDateTime, Timelike};
 
 /// English: The helper of hour
 ///
 /// 中文: 时助手
 pub trait HourHelper {
+    /// Add the specified number of hours
+    ///
+    /// 中文：加上指定的小时数
+    fn add_hours(&self, hour: u32) -> Self;
+
     /// English: Get the begin of one hour
     ///
     /// 中文: 获取小时的开始时间
@@ -13,7 +18,6 @@ pub trait HourHelper {
     ///
     /// 中文: 获取小时的结束时间
     fn end_of_hour(&self) -> Self;
-
     /// English: Is the same hour
     ///
     /// 中文: 判断两个时间是否在同一个小时
@@ -21,6 +25,11 @@ pub trait HourHelper {
 }
 
 impl HourHelper for NaiveDateTime {
+    fn add_hours(&self, hour: u32) -> Self {
+        self.checked_add_signed(Duration::hours(hour as i64))
+            .expect("Overflowed")
+    }
+
     fn begin_of_hour(&self) -> Self {
         let hour = self.hour();
         self.date().and_hms_opt(hour, 0, 0).unwrap()
@@ -68,5 +77,12 @@ mod tests {
         let one = gen_time(2000, 1, 1, 0, 0, 0).unwrap();
         let other = gen_time(2000, 1, 1, 1, 0, 0).unwrap();
         assert!(!one.is_same_hour(&other))
+    }
+    #[test]
+    fn test_add_hours() {
+        let one = gen_time(2000, 1, 1, 0, 0, 0);
+        let actual = gen_time(2000, 1, 1, 6, 0, 0);
+        let result = one.map(|date| date.add_hours(6));
+        assert_eq!(result, actual);
     }
 }
